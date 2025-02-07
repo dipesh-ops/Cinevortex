636c0681e60453411b457874124b2483e0a07f8d
dipesh-ops <124789358+dipesh-ops@users.noreply.github.com>
2025-02-07 11:44:26 +0530

    added header component and called api for trending movies and shows

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 
-const Header = () => {
+const Header = ({data}) => {
+    const {backdrop_path, poster_path, original_title, overview, title, original_name, media_type, first_air_date} = data
   return (
-    <div style={{
-        background : `linear-gradient(rgba(0.0.0.4),rgba(0.0.0.7),rgba(0.0.0.9))`
-    }} className="w-full h-[50vh]">
-        
+    <div className="w-full h-[50vh] relative">
+        <div>
+            <div className="absolute w-2/4 p-4 top-20 bg-linear-to-l from-transperent to-blue-500">
+            <h1 className="text-5xl font-bold mb-1">{original_title || title || original_name}</h1>
+            <p>{overview && overview.slice(0, 200)}</p>
+            <i className="text-yellow-200 ri-megaphone-fill mr-2"> {first_air_date || "No information"}</i>
+            <i className="text-yellow-200 ri-movie-2-fill"> {media_type}</i>
+            <div>
+            <button className="bg-[#0400ff] w-[10rem] p-2 shadow-lg rounded-2xl mt-2"><i className="ri-play-line"></i> Watch Trailer</button>
+            </div>
+            </div>
+            <img className="w-full h-[50vh]" src={`https://image.tmdb.org/t/p/original/${backdrop_path || poster_path}`} alt="" />
+        </div>
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react"
 import Header from "./Header"
 import Sidebar from "./Sidebar"
 import Topnav from "./Topnav"
+import axios from "../utils/axios"
 
 const Home = () => {
-  return (
+
+  const [wallpaper, setWallpaper] = useState([]);
+
+  const getHeaderWallpaper =async ()=>{
+    try {
+      const {data} = await axios.get(`/trending/all/day`);
+
+      const randomWalls = data.results[(Math.random() * data.results.length).toFixed()]
+      setWallpaper(randomWalls)
+    } catch (error) {
+      console.log("ERROR :", error);
+      
+    }
+  }
+
+  console.log(wallpaper);
+  
+
+  useEffect(()=>{
+    getHeaderWallpaper();
+  }, [])
+  return wallpaper ? (
     <div>
         <div className="h-[100vh] text-white flex">
         <div className="border-r-2 border-zinc-400 w-[20%] h-[100%]">
@@ -11,11 +34,11 @@ const Home = () => {
         </div>
         <div className="w-[80%] h-full">
             <Topnav/>
-            <Header/>
+            <Header data={wallpaper}/>
         </div>
       </div>
     </div>
-  )
+  ) : "Loading"
 }
 
 export default Home
