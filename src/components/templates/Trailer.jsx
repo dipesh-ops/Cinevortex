40057ca4e450bd1742197c0fbaf3966945f1d3d4
dipesh-ops <124789358+dipesh-ops@users.noreply.github.com>
2025-02-18 16:14:59 +0530

    updated trailer.jsx

--- src/components/templates/Trailer.jsx
+++ src/components/templates/Trailer.jsx
@@ -14,7 +14,7 @@ const Trailer = () => {
     
   return (
     <div className="z-[20] bg-black absolute w-screen h-screen top-0 left-0 flex items-center justify-center">
-        <Link onClick={()=> navigate(-1)} className="absolute right-5 top-85 md:right-20 md:top-20 text-2xl"><i className="ri-close-large-line"></i></Link>
+        <Link onClick={()=> navigate(-1)} className="absolute right-5 top-60 md:right-20 md:top-20 text-2xl"><i className="ri-close-large-line"></i></Link>
         {ytVideo ? <div className="md:h-[80%] md:w-[80%]"><ReactPlayer width="100%" height="100%" url={`https://www.youtube.com/watch?v=${ytVideo.key}`} /></div> : <NotFound/>}
     </div>
   )
