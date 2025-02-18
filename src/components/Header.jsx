22bae149a72b89f7b177c3a5a4983753c2d2a080
dipesh-ops <124789358+dipesh-ops@users.noreply.github.com>
2025-02-18 16:44:53 +0530

    making some changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = ({data}) => {
     <div className="w-full h-[50vh] relative">
         <div>
             <div className="absolute w-2/4 p-4 top-20 bg-linear-to-l from-transperent to-blue-500">
-            <h1 className="text-5xl font-bold mb-1">{original_title || title || original_name}</h1>
+            <h1 className="text-2xl md:text-5xl font-bold mb-1">{original_title || title || original_name}</h1>
             <p className="hidden md:hidden lg:block">{overview && overview.slice(0, 200)}</p>
             <i className="text-yellow-200 ri-megaphone-fill mr-2"> {first_air_date || "No information"}</i>
             <i className="text-yellow-200 ri-movie-2-fill"> {media_type}</i>
